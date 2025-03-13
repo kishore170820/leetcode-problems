1a32fd5a82e80f57d0e9272cc3f20bd25ffd439f
kishore170820 <ckishore907@gmail.com>
2025-03-14 01:24:16 +0530

    Time: 0 ms (100%), Space: 17.8 MB (65.02%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,6 +1,7 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
         res = [[]]
-        for num in nums:
-            res += [curr + [num] for curr in res]
+        for i in range(len(nums)):
+            for j in range(len(res)):
+                res.append(res[j] + [nums[i]])
         return res
